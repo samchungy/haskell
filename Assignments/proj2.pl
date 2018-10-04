350f7708cc6cc55b71926721571b6901fed9b02b
Sam Chung <samchungy@gmail.com>
2018-10-04 16:42:45 +1000

    done

--- Assignments/proj2.pl
+++ Assignments/proj2.pl
@@ -5,14 +5,19 @@
 % Project Explanation:
 % The project was written for a University of Melbourne subject. The aim of 
 % this project is to write prolog code in order to solve a crossword like 
-% puzzle called a fill-in puzzle. A more in-depth explanation of what a
-% fill in puzzle is can be found here: 
+% puzzle called a fill-in puzzle. A list of words is provided alongside a 
+% crossword puzzle and the objective is to fill this puzzle with the words
+% in a manner such that all the columns and rows make up the words.
+% A more in-depth explanation of what a fill in puzzle is can be found here: 
 % https://en.wikipedia.org/wiki/Fill-In_(puzzle).
 
 % The aim of this project was to create efficient code which will solve puzzles
 % of different sizes and complexities in under 20 seconds as tested by the
-% university on linux servers. Hence there are a number of efforts made to
-% optimise the efficiency of the code in the place of simplicity.
+% university on linux servers. If we wanted to do it extremely inefficiently,
+% we could test every single combination however for example: a 7 × 7 puzzle 
+% which may have 18 words can have 18! > 1015 permutations which would take an
+% extremely long time to process. Hence there are a number of efforts made to 
+% optimise the efficiency of the code in place of simplicity.
 
 % Terminology Used:
 % Pair : Pairs are used in SWI-Prolog in the format of key-value
@@ -98,7 +103,7 @@ valid_puzzle([Row|Rows]) :-
 % 3. Creating pairs which store Words with compatible slots. These are sorted
 %       to minimise the search space when we try to unify the words with slots.
 % 4. Unification by inserting words into slots.
-% 5. Returning the solve puzzle back to the main function.
+% 5. Returning the solve puzzle back to the main predicate.
 solve_puzzle(Puzzle, WordList, SolvedPuzzle) :-
         % Preparing the Puzzle for Solving
         init_variables(Puzzle, VarPuzzle),
@@ -119,7 +124,7 @@ solve_puzzle(Puzzle, WordList, SolvedPuzzle) :-
 	
 % Iterates over the puzzle and calls init_vars_row in order to process over
 % rows. Uses accumulators in order to store completed rows. The purpose of this
-% function is to replace '_' with free variables in order to enable unification
+% predicate is to replace '_' with free variables in order to enable unification
 % in prolog.
 init_variables(OldPuzzle, NewPuzzle) :-
 	init_variables(OldPuzzle, [], NewPuzzle).
@@ -152,7 +157,6 @@ create_slots(Puzzle, Slots) :-
 	append(HorizontalSlots, VerticalSlots, Slots).
 create_slots([], Slots, Slots).
 create_slots([Row|Rows], Prev, Slots) :-
-        % NewSlots is a list of slots.
 	create_slots_row(Row, [], [], NewSlots),
 	append(Prev, NewSlots, Acc2),
 	create_slots(Rows, Acc2, Slots).
@@ -166,10 +170,7 @@ create_slots([Row|Rows], Prev, Slots) :-
 % Stores the accumulating space size in Space_Size, 
 % Stores those complete slots in Prev
 create_slots_row([], Prev, Space_Size, Slots) :-
-	% Reached the end of the row
 	length(Space_Size, Len),
-        % We need to check the current space_size to see if
-        % we have another slot.
 	(  Len > 1
 	-> length(Prev, PrevLen),
 	   % Deal with Prev being empty to avoid returning a list instead of
@@ -186,8 +187,6 @@ create_slots_row([X|Xs], Prev, Space_Size, Slots) :-
 	length(Space_Size, Len),
         % nonvar ensures that the free variable doesn't get unified with #
 	(  (nonvar(X), X = '#')
-        % We've found a # meaning we have to check if our space size
-        % is larger than 1. If so, we add it to the slot accumulator.
 	-> (  Len > 1
 	   -> append(Prev, [Space_Size], Acc2),
               create_slots_row(Xs, Acc2, [], Slots)
@@ -208,14 +207,32 @@ create_slots_row([X|Xs], Prev, Space_Size, Slots) :-
 % Start Optimisation Functions 
 % ------------------------------------------------ %  
 
+% Converts a list of words and a list of slots into pairs of format
+% Word-[[Slot]] which represent a word and compatible slots which it fits.
+%
+% 1. Sort Wordlist & Slotlists by length
+% 2. Pack all items in both of the lists into lists of same length 
+%        eg. [[a,b],[c,d]],[[a,b,c],[d,e,f]] (Words)
+%           [[_,_],[_,_]],[[_,_,_],[_,_,_]] (Slots)
+% 3. Run a unifiability predicate on them and put them in pairs
+%    of format [Word-[UnifiableSlots]], where Word is a single word and 
+%    UnifiableSlots is a list of compatible slots.
+% 4. Sort the pairs by the length of UnifiableSlots. This minimises
+%        our search space when we try to solve the crossword.
 create_pairs(WordList, SlotList, SortedPairs) :-
+        % Sorting Lists
         quicksort_single(SlotList, SortedSlots),
         quicksort_single_mid_pivot(WordList, SortedWords),
+        % Packing Items of Same Length
         pack(SortedSlots, PackedSlots),
         pack(SortedWords, PackedWords),
+        % Pairing with unifiability predicate
         pair_words_with_slots(PackedWords, PackedSlots, Pairs),
+        % Sorting Again
         quicksort_pair_mid_pivot(Pairs, SortedPairs).
 
+% Takes the packedlists and passes 1 of each to pair_words_with_slots to
+% process. Accumulates the Pairs in FinalPairs and returns that back.
 pair_words_with_slots(PackedWords, PackedSlots, SortedPairs) :-
         pair_words_with_slots(PackedWords, PackedSlots, [], SortedPairs).
 pair_words_with_slots([],_, FinalPairs, FinalPairs).
@@ -224,11 +241,16 @@ pair_words_with_slots([WGroup|WGroups],[SGroup|SGroups], Acc, FinalPairs):-
         append(Pairs,Acc,Acc2),
         pair_words_with_slots(WGroups, SGroups, Acc2, FinalPairs).
 
+% Simply assumes that the packedlists are of the same length, passes it to 
+% test_word in order to test the unifiability of all the words against all the
+% slots and creates a list of pairs.
 pair_a_word_with_slots([],_,Pairs, Pairs).
 pair_a_word_with_slots([Word|Words],Slots, Acc, Pairs):-
         test_word(Word, Slots, UnifiableList),
         pair_a_word_with_slots(Words, Slots, [Word-UnifiableList|Acc], Pairs).
 
+% Tests the unifiability of Word with Slots without
+% unifying them! Returns a list of the unifiable slots
 test_word(Word, Slots, UnifiableList):-
         test_word(Word, Slots, [], UnifiableList).
 test_word(_,[],Slots,Slots).
@@ -238,7 +260,7 @@ test_word(Word, [Slot|Slots], Ys, List):-
         ; test_word(Word, Slots, Ys, List)
         ).
 
-% Pack function adapted from
+% Pack predicate adapted from
 % http://www.ic.unicamp.br/~meidanis/courses/problemas-prolog/p09.prolog
 % Packs repeated lengths of elements into sublists.
 pack([],[]).
@@ -259,12 +281,12 @@ transfer(W, [X|Xs], Ys, [W|Zs]) :-
         Len_W =:= Len_X,
         transfer(X, Xs, Ys, Zs).
 
-% -------------- Quick Sorts -------------- %
+% -------------- Start Quick Sorts -------------- %
 % Adapted from https://www.cp.eng.chula.ac.th/~piak/teaching/dsys/2004/
 % quick-prolog.htm
 
 % Used to grab the middle element for pivot selection in
-% the quicksort_pair function.
+% the quicksort_pair predicate.
 middle(List, Middle) :-
     middle(List, List, Middle).
 middle([M|_], [_,_], M).
@@ -272,7 +294,7 @@ middle([M|_], [_], M).
 middle([_|T], [_,_,X|T2], Middle) :-
     middle(T, [X|T2], Middle).
 
-% Starts the quicksort_pair algorithm with the middle element as the pivot
+% Starts the quicksort algorithm with the middle element as the pivot
 % This is great when we know a list is mostly sorted to avoid choosing the
 % worse case pivot.
 quicksort_single_mid_pivot(Xs,Ys):-
@@ -283,7 +305,7 @@ quicksort_single_mid_pivot(Xs,Ys):-
         append(Ls, Rs, Ys).
 quicksort_single_mid_pivot([],[]).
 
-% Regular quicksort_pair, chooses first item as pivot.
+% Regular quicksort, chooses first item as pivot.
 quicksort_single([X|Xs],Ys) :-
         partition_single(Xs,X,Left,Right),
         quicksort_single(Left,Ls),
@@ -305,34 +327,37 @@ partition_single([X|Xs],Y,Ls,[X|Rs]) :-
         partition_single(Xs,Y,Ls,Rs).
 partition_single([],_,[],[]).
 
-%
+% Same as single mid_pivot quicksort, used to call the pair quicksort
 quicksort_pair_mid_pivot(Xs,Ys):-
         middle(Xs, X),
-        partition(Xs, X, Left, Right),
+        partition_pair(Xs, X, Left, Right),
         quicksort_pair(Left, Ls),
         quicksort_pair(Right, Rs),
         append(Ls, Rs, Ys).
 quicksort_pair_mid_pivot([],[]).
 
+% Same as single quicksort, used to call the partion pairing.
 quicksort_pair([X|Xs],Ys) :-
-        partition(Xs,X,Left,Right),
+        partition_pair(Xs,X,Left,Right),
         quicksort_pair(Left,Ls),
         quicksort_pair(Right,Rs),
         append(Ls,[X|Rs],Ys).
 quicksort_pair([],[]).
 
-% Partitioning key value pairs eg. [a]-[_]
-partition([Xk-Xv|Xs],Yk-Yv,[Xk-Xv|Ls],Rs) :-
+% Partitioning key value pairs eg. [Word]-[[Slot]]. Compares the Slots based 
+% on their lengths and partitions them to the Left and right accordingly.
+partition_pair([Xk-Xv|Xs],Yk-Yv,[Xk-Xv|Ls],Rs) :-
         length(Yv, Y_Len),
         length(Xv, X_Len),
         X_Len =< Y_Len,
-        partition(Xs,Yk-Yv,Ls,Rs).
-partition([Xk-Xv|Xs],Yk-Yv,Ls,[Xk-Xv|Rs]) :-
+        partition_pair(Xs,Yk-Yv,Ls,Rs).
+partition_pair([Xk-Xv|Xs],Yk-Yv,Ls,[Xk-Xv|Rs]) :-
         length(Yv, Y_Len),
         length(Xv, X_Len),
         X_Len > Y_Len,
-        partition(Xs,Yk-Yv,Ls,Rs).
-partition([],_,[],[]).
+        partition_pair(Xs,Yk-Yv,Ls,Rs).
+partition_pair([],_,[],[]).
+% -------------- End Quick Sorts -------------- %
 
 % ------------------------------------------------ %
 % End Optimisation Functions 
@@ -342,55 +367,37 @@ partition([],_,[],[]).
 % Start Insertion Functions
 % ------------------------------------------------ %
 
-pairinsertionSort([], []).
-
-pairinsertionSort([HEAD|TAIL], RESULT) :-
-   pairinsertionSort(TAIL, LIST), pairinsertInPlace(HEAD, LIST, RESULT).
-
-%-----------------------------------------------------------------------
-% insertInPlace(ELEMENT, LIST, RESULT)
-% RESULT is the result of inserting ELEMENT into sorted list LIST
-% at the correct place.
-%-----------------------------------------------------------------------
-
-pairinsertInPlace(ELEMENT, [], [ELEMENT]).
-pairinsertInPlace(Word-Slots, [Word2-Slots2|TAIL], [Word-Slots|LIST]) :-
-        length(Slots, Slots_Len),
-        length(Slots2, Slots2_Len),
-        Slots_Len =< Slots2_Len, pairinsertInPlace(Word2-Slots2, TAIL, LIST).
-
-pairinsertInPlace(Word-Slots, [Word2-Slots2|TAIL], [Word2-Slots2|LIST]) :-
-        length(Slots, Slots_Len),
-        length(Slots2, Slots2_Len),
-        Slots_Len > Slots2_Len, pairinsertInPlace(Word-Slots, TAIL, LIST).
-
-% Pair is in Key Value Pair Format eg. [Word]-[[Slot]]
+% Inserts words until the crossword is filled. Attempts to unify the first word
+% with the first slot.
 insert_words([]).
 insert_words([Word-[Slot|Slots]|Pairs]):-
-        length([Slot|Slots],SlotLength),
-        SlotLength > 0, % This ensures a quick fail.
-        (  (Word = Slot, prune_pairs(Pairs) ) 
+        % Attempt to unify, Check validity of unification.
+        (  (Word = Slot, update_pairs(Pairs) ) 
         % Successful Unification
-        -> true  % Do absolutely nothing
-        ; % Failed Unification, Try and test it on the other compatible slots.
+        -> true 
+        ; % Failed Unification, Try and test against other slots.
            length(Slots, RemainingLen),
+           % Fast Failure - No more slots to try.
            RemainingLen > 0,
            insert_words([Word-Slots|Pairs])
         ).
         
 
-% Since we've unified a slot with a word, we have to remove this slot from
-% the other pairs.
-prune_pairs(Pairs):-
-        prune_pairs(Pairs, []).
-prune_pairs([],Pruned) :-
+% Since we've unified a slot with a word, we have to go back into the pairs
+% and test their unifiability with the new slots.
+update_pairs(Pairs):-
+        update_pairs(Pairs, []).
+update_pairs([],Pruned) :-
+        % Re-Sort updated list to place word with least slots up the top.
         quicksort_pair_mid_pivot(Pruned, SortedPruned),
         insert_words(SortedPruned).
-prune_pairs([Word-Slots|Pairs], Acc):-
+update_pairs([Word-Slots|Pairs], Acc):-
+        % Test unifiability of word against the updated slots.
         test_word(Word, Slots, Remaining),
         length(Remaining, NumRemaining),
+        % Fast Failure - 0 matching slots.
         NumRemaining > 0,
-        prune_pairs(Pairs, [Word-Remaining|Acc]).
+        update_pairs(Pairs, [Word-Remaining|Acc]).
 % ------------------------------------------------ %
 % End Insertion Functions 
 % ------------------------------------------------ %  
